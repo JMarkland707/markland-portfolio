386ebd6c88cb36af3eefbf8c061913d1301a8752
Justin <jkmarkland707@gmail.com>
2022-09-15 15:52:24 -0700

    Create round functions

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,9 +1,7 @@
 
 
-/* When a function (getComputerChoice) is called the computer will randomly return one of three values (rock, paper, scissors)*/
 
 /* Write a program that will randomly return a single choice of rock,paper,scissor.
-Computer only game that does not require user input or user selection yet
 If rock and paper then display message "paper  beats rock"
 if rock and scissors then display message "rock beats scissors"
 If rock and rock then display message "tie, try again"
@@ -11,15 +9,33 @@ If rock and rock then display message "tie, try again"
 
 
 /* Create three values for the computer to return */
-let gameChoices = ["rock", "paper", "scissors"];
+const gameChoices = ["rock", "paper", "scissors"];
 
+
+/* When a function (getComputerChoice) is called the computer will randomly return one of three values 
+(rock, paper, scissors)*/
+// Get computer Choice
 function getComputerChoice() {
-    let randomChoice = gameChoices[Math.floor(Math.random()*gameChoices.length)];
-    console.log("Rock, Paper, Scissors..BOOM!");
-    console.log(randomChoice);
-    console.log("Rock, Paper, Scissors..BOOM!");
-    console.log(randomChoice);
-
+    const randomChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+   // maybe needs to move into playround function
+    console.log("Computer selects " + randomChoice);
 }
 
-getComputerChoice();
+// Get player input
+function getPlayerChoice() {
+    let playerSelection = prompt("Choose Rock, Paper or Scissors");
+// prompt input will be stored in playerSelection, this needs to be converted to lowercase and then use this to compare against comp choice
+  console.log("You picked " + playerSelection.toLowerCase());
+}
+
+
+//Combine playerchoice and computer choice into rounds.  Call this function playRound
+
+function playRound(){
+    console.log("Rock, Paper, Scissors..BOOM!");
+    getPlayerChoice();
+    getComputerChoice();
+// Create outcomes of matches here
+
+}
+playRound();
